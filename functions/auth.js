50086b7b660d6e0c40070300aa88c2b973b433ee
Elichai <elichaimejia@gmail.com>
2026-03-08 21:01:03 -0600

    Create auth.js

---
+++ functions/auth.js
@@ -0,0 +1,25 @@
+export async function onRequest(context) {
+  const { request, env } = context;
+  const url = new URL(request.url);
+  const code = url.searchParams.get("code");
+
+  if (!code) return new Response("No code", { status: 400 });
+
+  const response = await fetch("https://github.com/login/oauth/access_token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({
+      client_id: env.GITHUB_CLIENT_ID,
+      client_secret: env.GITHUB_CLIENT_SECRET,
+      code: code,
+    }),
+  });
+
+  const data = await response.json();
+  return new Response(JSON.stringify(data), {
+    headers: { "Content-Type": "application/json" },
+  });
+}
